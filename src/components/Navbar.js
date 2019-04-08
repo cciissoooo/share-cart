60c5ccf5336426e93761ee1dd59643f18aee573f
Chuyu Liu <chuyu@dropbox.com>
2019-04-08 17:34:06 -0400

    add event button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link, NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
   return (
-    <nav className="nav-wrapper red darken-3">
+    <nav className="nav-wrapper purple lighten-3">
       <div className="container">
         <Link className="brand-logo" to="/">Hack Week</Link>
         <ul className="right">
